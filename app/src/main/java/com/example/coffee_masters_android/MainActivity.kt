b6af4a38711d1b02564ba2cebf5c2c2feb875f04
Ali Raza <ialidar2001@gmail.com>
2023-01-27 19:56:24 +0500

    Started The Application

---
+++ app/src/main/java/com/example/coffee_masters_android/MainActivity.kt
@@ -0,0 +1,44 @@
+package com.example.coffee_masters_android
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.coffee_masters_android.ui.theme.Coffee_Masters_AndroidTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            Coffee_Masters_AndroidTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+                    Text("")
+//                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    Coffee_Masters_AndroidTheme {
+        Greeting("Android")
+    }
+}
