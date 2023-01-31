6033effef1b8aa165f110b7f54df3e4fbb2a8751
Ali Raza <ialidar2001@gmail.com>
2023-01-31 23:53:58 +0500

    Added Offers page and Colors for the App

--- app/src/main/java/com/example/coffee_masters_android/OffersPage.kt
+++ app/src/main/java/com/example/coffee_masters_android/OffersPage.kt
@@ -1,15 +1,26 @@
 package com.example.coffee_masters_android
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.example.coffee_masters_android.ui.theme.Alternative1
+import com.example.coffee_masters_android.ui.theme.Alternative2
 
-@Preview(showBackground = true)
+@Preview(showBackground = true, widthDp = 400)
 @Composable
 fun Offers() {
 
@@ -18,8 +29,17 @@ fun Offers() {
         fontWeight = FontWeight.Normal,
         fontSize = 16.sp
     )
-    Column() {
-        Text(text = "My Title", fontSize = 16.sp)
-        Text(text = "Description", style = style)
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = Modifier.padding(16.dp),
+    ) {
+        Text(text = "My Title", style = MaterialTheme.typography.h5,
+            modifier = Modifier
+                .background(Alternative1)
+                .padding(16.dp))
+        Spacer(modifier = Modifier.height(16.dp))
+        Text(text = "Description", style = MaterialTheme.typography.h6,modifier = Modifier
+            .background(Alternative2)
+            .padding(16.dp))
     }
 }
