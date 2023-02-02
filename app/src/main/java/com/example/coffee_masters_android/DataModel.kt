60abb5991fcdf805e184e255d877f0f6a7ead8f7
Ali Raza <ialidar2001@gmail.com>
2023-02-02 20:11:25 +0500

    Added DataModels and Designed the Menu Page

--- app/src/main/java/com/example/coffee_masters_android/DataModel.kt
+++ app/src/main/java/com/example/coffee_masters_android/DataModel.kt
@@ -1,2 +1,15 @@
 package com.example.coffee_masters_android
 
+
+class Product(
+    var id:Int,
+    var name:String,
+    var price:Double,
+    var image:String
+){
+    val imageUrl get() = "https://firtman.github.io/coffeemasters/api/images/${this.image}"
+}
+
+class Category(var name:String, products:MutableList<Product>)
+
+class  ItemInCart(var product:Product,quantity:Int)
--- app/src/main/java/com/example/coffee_masters_android/pages/MenuPage.kt
+++ app/src/main/java/com/example/coffee_masters_android/pages/MenuPage.kt
@@ -1,10 +1,86 @@
 package com.example.coffee_masters_android.pages
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Button
+import androidx.compose.material.ButtonDefaults
+import androidx.compose.material.Card
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.BlendMode.Companion.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.coffee_masters_android.Product
+import com.example.coffee_masters_android.R
+import com.example.coffee_masters_android.ui.theme.Alternative1
+import com.example.coffee_masters_android.ui.theme.CardBackground
 
 @Preview
 @Composable
 fun MenuPage() {
+    LazyColumn{
+        items(5){
+            Card(
+                elevation = 2.dp,
+                shape = RoundedCornerShape(12.dp),
+                modifier = Modifier.background(CardBackground).padding(12.dp)
+            ) {
+                ProductItem(product = Product(1,"Ali",24.0,""), onAdd = {})
+            }
+        }
+    }
     
+}
+
+
+
+fun Double.format(digits: Int) = "%.${digits}f".format(this)
+
+
+@Composable
+fun ProductItem(product: Product, onAdd: (Product)->Unit) {
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .background(androidx.compose.ui.graphics.Color.White)
+    ) {
+
+        Image(
+            painter = painterResource(id = R.drawable.black_coffee),
+            contentDescription = "Image for ${product.name}",
+            contentScale = ContentScale.FillWidth,
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(180.dp)
+        )
+        Row(
+            horizontalArrangement = Arrangement.SpaceBetween,
+            modifier = Modifier
+                .padding(16.dp)
+                .fillMaxWidth()
+        ) {
+            Column {
+                Text(product.name, fontWeight = FontWeight.Bold)
+                Text("$${product.price.format(2)} ea")
+            }
+            Button(
+                colors = ButtonDefaults.buttonColors(
+                    backgroundColor = Alternative1,
+                    contentColor = androidx.compose.ui.graphics.Color.White
+                ),
+                onClick = {
+                    onAdd(product)
+                },
+            ) {
+                Text("Add")
+            }
+        }
+    }
 }
